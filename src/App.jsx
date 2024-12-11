b9c7077b4c34b6eac447915409a9e760f3f83019
Mohammed Umar khan <mohammedumarkhan803@gmail.com>
2024-12-11 18:41:36 +0530

    Home and technology page done

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,25 @@ import Navbar from './components/navbar/Navbar'
 import Conditions from './components/footer/footerLink/Conditions'
 import Footer from './components/footer/Footer'
 import Policies from './components/footer/footerLink/Policies'
+import Technology from "./components/pages/technology/Technology";
+
 
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/Contact" element={<Contact />} />
-    <Route path="/About" element={<About />} />
-    <Route path="/Conditions" element={<Conditions />} /> 
-    <Route path="Policies" element={<Policies />} />
-    </Routes>
-    <Footer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Conditions" element={<Conditions />} /> 
+        <Route path="/Policies" element={<Policies />} />
+        <Route path="/Technology" element={<Technology />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
