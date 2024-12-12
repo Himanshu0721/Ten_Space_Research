7bcd27a54ed06f86e682890e6a0a664a7d403a6c
robinbishnoi390@gmail.com <robinbishnoi390@gmail.com>
2024-12-12 13:23:15 +0530

    commit by Robin

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/pages/home/Home'
 import Contact from './components/pages/contact/Contact'
 import About from './components/pages/about/About'
+// import Book from './components/booklaunch/Book'
 import Book from './components/booklaunch/Book'
 
 function App() {
