1962fece9323ac7f36cf488ade35648b0e5c729c
robinbishnoi390@gmail.com <robinbishnoi390@gmail.com>
2024-12-12 13:17:20 +0530

    commit by Robin

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/pages/home/Home'
 import Contact from './components/pages/contact/Contact'
 import About from './components/pages/about/About'
+import Book from './components/booklaunch/Book'
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
     <Route path="/" element={<Home />} />
     <Route path="/contact" element={<Contact />} />
     <Route path="/about" element={<About />} />
+    <Route path="/book" element={<Book />} />
+
     </Routes>
     </BrowserRouter>
   )
