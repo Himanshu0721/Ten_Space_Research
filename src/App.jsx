9dba18c8ddf24a20cbedce37cc993e8aa9d98879
robinbishnoi390@gmail.com <robinbishnoi390@gmail.com>
2024-12-13 19:15:03 +0530

    commit by Robin

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Conditions from './components/footer/footerLink/Conditions'
 import Footer from './components/footer/Footer'
 import Policies from './components/footer/footerLink/Policies'
 import Technology from "./components/pages/technology/Technology";
+import Book from "./components/booklaunch/Book"
 
 
 
@@ -22,6 +23,7 @@ function App() {
         <Route path="/Conditions" element={<Conditions />} /> 
         <Route path="/Policies" element={<Policies />} />
         <Route path="/Technology" element={<Technology />} />
+        <Route path="/Book" element={<Book />} />
       </Routes>
       <Footer />
     </BrowserRouter>
